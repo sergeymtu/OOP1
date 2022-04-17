17d0a4c9fc850276a415d0c80b799840ecdba8db
sergeymtu <sergeym@bk.ru>
2022-04-17 18:43:07 +0500

    second commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -2,11 +2,5 @@ package ru.netology
 
 
 fun main() {
-    val amount: Int = 4_699_00;
-//    val resultCommission = if (amount > 4_668_00) amount/10000*75  else 3500
-//    println("Сумма комиссии с перевода: $resultCommission")
-    val comission = amount / 10000 * 75
-    val resultComission = if (comission > 3500) comission else 3500
 
-    println("$resultComission")
 }
